350ead9a1e04637ecf9e9223f55f97b0625d0899
DimitriRomano <dimitri.romano09@gmail.com>
2020-12-29 17:18:05 +0100

    modif test

--- app/src/main/java/com/romano/dimitri/touristapp/model/Place.java
+++ app/src/main/java/com/romano/dimitri/touristapp/model/Place.java
@@ -1,5 +1,73 @@
 package com.romano.dimitri.touristapp.model;
 
-public class Place {
+import java.io.Serializable;
 
+public class Place implements Serializable {
+    private String mTitle;
+    private String mType;
+    private Double mLatitude;
+    private Double mLongitude;
+    private String mDescription;
+    private boolean mVisited;
+
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getType() {
+        return mType;
+    }
+
+    public void setType(String type) {
+        mType = type;
+    }
+
+    public Double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        mLatitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        mLongitude = longitude;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public boolean isVisited() {
+        return mVisited;
+    }
+
+    public void setVisited(boolean visited) {
+        mVisited = visited;
+    }
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mType='" + mType + '\'' +
+                ", mLatitude=" + mLatitude +
+                ", mLongitude=" + mLongitude +
+                ", mDescription='" + mDescription + '\'' +
+                ", mVisited=" + mVisited +
+                '}';
+    }
 }
