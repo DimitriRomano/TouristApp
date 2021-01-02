2b40f63e482a0f50618ad69a67c6ec7f4d6948b7
aladd <aladdineben@outlook.com>
2021-01-02 16:57:33 +0100

    Add image when registering, but it still struggle, so it doesnt work

--- app/src/main/java/com/romano/dimitri/touristapp/UserFragment.java
+++ app/src/main/java/com/romano/dimitri/touristapp/UserFragment.java
@@ -69,8 +69,9 @@ public class UserFragment extends Fragment {
         mGradeView.setText(mGrade);
         mScoreView.setText(mScore + "/10000 XP");
         mAgeView.setText(mAge + " ans");
-        //System.out.println("Bitmap" + BitmapFactory.decodeByteArray(mImage,0,mImage.length).toString());
-        //mImageProfile.setImageBitmap(BitmapFactory.decodeByteArray(mImage,0,mImage.length));
+       // System.out.println("Bitmap" + BitmapFactory.decodeByteArray(mImage,0,mImage.length).toString());
+        System.out.println(mImage.length);
+        mImageProfile.setImageBitmap(BitmapFactory.decodeByteArray(mImage,0,mImage.length));
         calculateScore(view);
     }
 
