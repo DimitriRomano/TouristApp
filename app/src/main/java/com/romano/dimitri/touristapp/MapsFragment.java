b8ee39891dbcc9bf34a09d80f8f22eb5366ef78a
DimitriRomano <dimitri.romano09@gmail.com>
2021-01-03 22:42:05 +0100

    alladine modif

--- app/src/main/java/com/romano/dimitri/touristapp/MapsFragment.java
+++ app/src/main/java/com/romano/dimitri/touristapp/MapsFragment.java
@@ -29,9 +29,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.romano.dimitri.touristapp.model.Place;
+
+import java.util.ArrayList;
 
 import static android.content.Context.LOCATION_SERVICE;
 
